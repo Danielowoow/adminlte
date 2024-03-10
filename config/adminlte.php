5049605cf68ca62390d0b5e97811c2aebf1287c1
Carlos Daniel Quispe Quispe <quispecarlosdaniel078@gmail.com>
2024-03-09 22:22:55 -0500

    xd

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Panel</b>ADMIN',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
@@ -159,7 +159,7 @@ return [
     'layout_dark_mode' => true,
 
     /*
-|------------------------------------------------- -------------------------
+    |------------------------------------------------- -------------------------
     | Clases de vistas de autenticación
     |------------------------------------------------- -------------------------
     |
@@ -215,7 +215,7 @@ return [
     'sidebar_collapse_auto_size' => false,
     'sidebar_collapse_remember' => false,
     'sidebar_collapse_remember_no_transition' => true,
-    'sidebar_scrollbar_theme' => 'os-theme-light',
+    'sidebar_scrollbar_theme' => 'os-theme-dark',
     'sidebar_scrollbar_auto_hide' => 'l',
     'sidebar_nav_accordion' => true,
     'sidebar_nav_animation_speed' => 300,
